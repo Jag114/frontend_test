aa8158072e9f81fbf67748ff8731273b804cd244
jag114 <jag200202@gmail.com>
2024-07-13 19:35:40 +0200

    create angular project

---
+++ zadanie/src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  count: number = 0;
+
+  increment() {
+    this.count++;
+  }
+}
