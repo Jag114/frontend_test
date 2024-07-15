5edb1bc2fdc4d94dd6460aff53c2ffd1fce54419
jag114 <jag200202@gmail.com>
2024-07-15 15:30:36 +0200

    basic styling done

--- zadanie/src/app/app.component.ts
+++ zadanie/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from "./components/header/header.component"
-import { WrapperComponent } from './wrapper/wrapper.component';
-import { FooterComponent } from './footer/footer.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
