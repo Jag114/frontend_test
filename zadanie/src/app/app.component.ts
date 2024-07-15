d08e47fac8fea190c0899f365b9760f3fd78adfa
jag114 <jag200202@gmail.com>
2024-07-15 10:19:14 +0200

    header done, left styling done

--- zadanie/src/app/app.component.ts
+++ zadanie/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from "./components/header/header.component"
+import { WrapperComponent } from './wrapper/wrapper.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [HeaderComponent, WrapperComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- zadanie/src/app/wrapper/wrapper.component.ts
+++ zadanie/src/app/wrapper/wrapper.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+import { RightComponent } from '../right/right.component';
+import { MiddleComponent } from '../middle/middle.component';
+import { LeftComponent } from '../left/left.component';
+
 @Component({
   selector: 'app-wrapper',
   standalone: true,
-  imports: [],
+  imports: [RightComponent, MiddleComponent, LeftComponent],
   templateUrl: './wrapper.component.html',
   styleUrl: './wrapper.component.scss'
 })
